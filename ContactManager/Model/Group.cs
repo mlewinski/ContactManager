70557ebd34fa327662beb989b7dd1e00f77dfddc
Marek Lewiński <lewinski.marek@outlook.com>
2017-07-17 21:49:06 +0200

    Implemented Group model

--- ContactManager/Model/Group.cs
+++ ContactManager/Model/Group.cs
@@ -8,5 +8,20 @@ namespace ContactManager.Model
 {
     public class Group
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Contact> Members { get; set; }
+
+        public void AddContact(Contact contact)
+        {
+            if(contact!=null)
+                Members.Add(contact);
+        }
+
+        public void RemoveContact(int ID)
+        {
+            Members.RemoveAll(x => x.ID == ID);
+        }
     }
 }
